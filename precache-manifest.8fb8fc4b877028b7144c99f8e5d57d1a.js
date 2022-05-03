0728bda41db168e643479cffa5f778445b9b943d
sumit-dhyani <dhyanisumit3@gmail.com>
2022-05-03 13:12:23 +0530

    Updates

---
+++ precache-manifest.8fb8fc4b877028b7144c99f8e5d57d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3777cbecaacd33315247",
+    "url": "/Robo_Friends/static/css/main.d54b3e64.chunk.css"
+  },
+  {
+    "revision": "3777cbecaacd33315247",
+    "url": "/Robo_Friends/static/js/main.3777cbec.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Robo_Friends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "71de685ec3ae0bee6f45",
+    "url": "/Robo_Friends/static/css/2.f02a1d8b.chunk.css"
+  },
+  {
+    "revision": "71de685ec3ae0bee6f45",
+    "url": "/Robo_Friends/static/js/2.71de685e.chunk.js"
+  },
+  {
+    "revision": "95f79ee38c4988d4dbcc",
+    "url": "/Robo_Friends/static/js/runtime~main.95f79ee3.js"
+  },
+  {
+    "revision": "8bccd261114d98159d8f7ddd457e53b8",
+    "url": "/Robo_Friends/index.html"
+  }
+];
